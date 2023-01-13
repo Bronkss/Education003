1564ca6139f42dc81c9c6652f0d74a5018a9fbf4
Alex <alexandr.yakimov.heaven@gmail.com>
2023-01-13 14:57:52 +0500

    Приветсвие

--- Education003/Program.cs
+++ Education003/Program.cs
@@ -11,7 +11,7 @@ namespace Education003
         static void Main(string[] args)
         {
             Console.WriteLine("Привет!");
+            Console.WriteLine("Это сообщение видно?");
         }
     }
 }
-#if
